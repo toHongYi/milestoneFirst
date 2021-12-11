d9312258a2b4c8a52752b1ecc6edfbb043b49226
hongyi <the_hongyi@163.com>
2021-12-11 13:29:40 +0800

    伟大的第一次

---
+++ HelloWorld/src/main/java/com/hongyi/controller/demo1.java
@@ -0,0 +1,14 @@
+package com.hongyi.controller;
+
+/**
+ * @version : V1.0
+ * @date : 2021/12/11 13:28
+ * @Author : LLH
+ * @Desc :
+ */
+public class demo1 {
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+    }
+}
