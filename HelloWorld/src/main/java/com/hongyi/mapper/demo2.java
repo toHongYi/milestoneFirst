1a9a55bd151bccf22b850ec1068b7bb54feed478
hongyi <the_hongyi@163.com>
2021-12-11 13:44:14 +0800

    第二次了

---
+++ HelloWorld/src/main/java/com/hongyi/mapper/demo2.java
@@ -0,0 +1,13 @@
+package com.hongyi.mapper;
+
+/**
+ * @version : V1.0
+ * @date : 2021/12/11 13:39
+ * @Author : LLH
+ * @Desc :
+ */
+public class demo2 {
+    public static void main(String[] args) {
+        System.out.println("我们可以庆祝下的");
+    }
+}
